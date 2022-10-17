6e65553eb7ba432dc60964ab0ffab04adc0a824f
Andrea L <e0772610@u.nus.edu>
2022-10-17 16:41:43 +0800

    CarModel session bean, usecases #14 to #17 (pls help to see if its carlist i should use for retrieve)

---
+++ CaRMS-ejb/src/java/ejb/session/stateless/CarModelSessionBeanLocal.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import entity.CarModel;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Andrea
+ */
+@Local
+public interface CarModelSessionBeanLocal {
+
+    public CarModel createCarModel(CarModel carModel);
+
+    public List<CarModel> retrieveAllCarModels();
+
+    public CarModel updateCarModel(CarModel carModel);
+
+    public CarModel retrieveCarModel(long carModelId);
+
+    public boolean deleteCarModel(long carModelId);
+    
+}
