2cb52b1f7a47f41eea59b933be8d70bb42743d9f
Andrea L <e0772610@u.nus.edu>
2022-10-17 16:42:15 +0800

    CarModel session bean, usecases #14 to #17 (pls help to see if its carlist i should use for retrieve)

--- CaRMSLibrary/src/entity/CarModel.java
+++ CaRMSLibrary/src/entity/CarModel.java
@@ -12,19 +12,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 /**
  *
  * @author Uni
  */
 @Entity
-public class Model implements Serializable {
+public class CarModel implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long modelId;
+    private Long carModelId;
 
     @Column(nullable = false, length = 32)
     private String make;
@@ -32,13 +35,25 @@ public class Model implements Serializable {
     private String model;
     @Column(nullable = false)
     private boolean enabled;
+    
+    @ManyToOne(optional = false)
+    @JoinColumn(nullable = false)
+    private CarCategory carCategory;
 
     @OneToMany(mappedBy = "model")
     private List<Car> carList;
 
-    public Model() {
+
+    public CarModel() {
         this.enabled = true;
     }
+
+    public CarModel(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+    
+    
 
     public boolean isEnabled() {
         return enabled;
@@ -72,29 +87,29 @@ public class Model implements Serializable {
         this.carList = carList;
     }
 
-    public Long getModelId() {
-        return modelId;
+    public Long getCarModelId() {
+        return carModelId;
     }
 
-    public void setModelId(Long modelId) {
-        this.modelId = modelId;
+    public void setCarModelId(Long carModelId) {
+        this.carModelId = carModelId;
     }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (modelId != null ? modelId.hashCode() : 0);
+        hash += (carModelId != null ? carModelId.hashCode() : 0);
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the modelId fields are not set
-        if (!(object instanceof Model)) {
+        // TODO: Warning - this method won't work in the case the carModelId fields are not set
+        if (!(object instanceof CarModel)) {
             return false;
         }
-        Model other = (Model) object;
-        if ((this.modelId == null && other.modelId != null) || (this.modelId != null && !this.modelId.equals(other.modelId))) {
+        CarModel other = (CarModel) object;
+        if ((this.carModelId == null && other.carModelId != null) || (this.carModelId != null && !this.carModelId.equals(other.carModelId))) {
             return false;
         }
         return true;
@@ -102,7 +117,15 @@ public class Model implements Serializable {
 
     @Override
     public String toString() {
-        return "entity.Model[ id=" + modelId + " ]";
+        return "entity.Model[ id=" + carModelId + " ]";
+    }
+
+    public CarCategory getCarCategory() {
+        return carCategory;
+    }
+
+    public void setCarCategory(CarCategory carCategory) {
+        this.carCategory = carCategory;
     }
 
 }
